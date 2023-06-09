2d315ac3c3772d9faf1be9747e4a06b0fa7030f1
TaccLuc <tacc.luca@gmail.com>
2023-06-09 14:06:57 +0200

    Fix: pwd correction

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ let userName = prompt('Inserisci il tuo nome');
 let userSurname = prompt('Inserisci il tuo cognonome');
 let userColor = prompt('Inserisci il tuo colore preferito');
 
-let pwd = userName + userSurname + userColor;
+let pwd = userName + userSurname + userColor + '101';
 
-document.getElementById('password').innerHTML = 'La tua password sicurissima è: ' + pwd + '101';
+document.getElementById('password').innerHTML = 'La tua password sicurissima è: ' + pwd;
